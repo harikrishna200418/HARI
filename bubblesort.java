1731a0394f69ead42bd7298383ff0b05966ae4ef
harikrishna200418 <harikrishna200418@gmail.com>
2025-06-04 11:00:10 +0530

    Add files via upload

---
+++ bubblesort.java
@@ -0,0 +1,27 @@
+public class bubblesort{
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {2, 4, 7, 9, 6};
+
+        // Bubble sort logic
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        // Print sorted array
+        printArray(arr);
+    }
+}
